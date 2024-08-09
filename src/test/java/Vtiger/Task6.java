f208422c9ebd16bcb52a80e07ac058e773444d58
anild <anild@Anil.admin.qsp>
2024-08-09 11:04:04 +0530

    final_mock_task_completed

---
+++ src/test/java/Vtiger/Task6.java
@@ -0,0 +1,36 @@
+package Vtiger;
+
+import static org.testng.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.testng.annotations.Test;
+
+import BaseClass.BaseVtiger;
+import FileUtility.ExcelUtil;
+import JavaUtility.JavaUtil;
+import POM_DWSVtiger.Home;
+import POM_DWSVtiger.Organizations;
+
+public class Task6 extends BaseVtiger{
+
+	ExcelUtil eUtil = new ExcelUtil();
+	@Test
+	public void createOrganization() throws EncryptedDocumentException, IOException
+	{
+		Home h = new Home(driver);
+		h.organizations();
+		
+		int row = 0;
+		Organizations org = new Organizations(driver);
+		org.plusIcon();
+		String orgName= eUtil.getData("Org", row++, 0);
+		org.organizationName(orgName+JavaUtil.generateRandomNumber(1000));
+		org.group();
+		org.assignedto(eUtil.getData("Org", row++, 0));
+		org.saveBtn();
+		
+		assertTrue((org.getCreatedOrgText().getText()).contains(orgName));
+	}
+}
